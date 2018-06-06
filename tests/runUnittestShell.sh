d0cd4ff1c0e84b6746e2fac3a6c52f5b9b1ed6eb
ArturoLara <arturo.lara@live.u-tad.com>
2018-06-06 23:59:01 +0200

    add and correct sh

--- tests/runUnittestShell.sh
+++ tests/runUnittestShell.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-
-
 find . -name "tests*.py" -print | while read f; do
         echo "$f"
         ###
-        python "$f"
+        python2 "$f"
         ###
-cd /var/lib/jenkins/workspace/test
-python manage.py runserver 0.0.0.0:8000 &
 done
