0f16ed458d26f8a279ed2decc269dd47d9cda833
ArturoLara <arturo.lara@live.u-tad.com>
2018-06-06 20:46:05 +0200

    add a run display to run chromedriver tests

--- tests/features/terrain.py
+++ tests/features/terrain.py
@@ -1,10 +1,14 @@
 from lettuce import world, before, after
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from pyvirtualdisplay import Display
 @before.all
 def setup_browser():
     # world.driver = webdriver.Chrome('chromedriver.exe')
+
+    display = Display(visible=0, size=(800, 800))
+    display.start()
+
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_driver_binary = "/usr/local/bin/chromedriver"
