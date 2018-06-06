82753f415b843a61d843c8391102ba8bf7623ed3
ArturoLara <arturo.lara@live.u-tad.com>
2018-06-06 21:14:00 +0200

    trying test with vistual display

--- tests/features/terrain.py
+++ tests/features/terrain.py
@@ -1,16 +1,20 @@
 from lettuce import world, before, after
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from pyvirtualdisplay import Display
 @before.all
 def setup_browser():
     # world.driver = webdriver.Chrome('chromedriver.exe')
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_driver_binary = "/usr/local/bin/chromedriver"
-    chrome_options.binary_location = '/opt/google/chrome/google-chrome'
-    world.driver = webdriver.Chrome(chrome_driver_binary, chrome_options=chrome_options)
+    display = Display(visible=0, size=(800, 800))
+    display.start()
+
+    #chrome_options = Options()
+    #chrome_options.add_argument("--headless")
+    #chrome_driver_binary = "/usr/local/bin/chromedriver"
+    #chrome_options.binary_location = '/opt/google/chrome/google-chrome'
+    #world.driver = webdriver.Chrome(chrome_driver_binary, chrome_options=chrome_options)
+    world.driver = webdriver.Chrome()
     world.driver.get("http://35.180.103.245:8000/")
 
 
